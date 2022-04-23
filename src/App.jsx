aca34a484127386191b34ee9ac881c03c3526966
Lorraine <lorraine.marie@live.ca>
2022-04-22 20:09:11 -0400

    ADD: Getting google maps set up

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,14 @@ import React, {Component} from 'react'
 
 import Yelp from './components/Yelp/Yelp.js'
 
+import Map from './components/Map/Map';
+
 export default class App extends Component {
   render() {
    return (
       <>
       <Yelp />
+      <Map />
       </>
     )
     
--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,11 +1,30 @@
 import React from 'react'
+import {GoogleMap, withScriptjs, withGoogleMap} from 'react-google-maps';
 
-function Map(){
-  
-}
+function Map() {
 
-export default function Map() {
+
   return (
-    <div>Map</div>
-  )
+    <GoogleMap 
+        defaultZoom={10} 
+        defaultCenter={{ lat: 12, lng: 12 }} />
+  );
+  }
+
+const WrappedMap = withScriptjs(withGoogleMap(Map));
+
+export default function App() {
+return (
+  <div>
+    <WrappedMap googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${
+          process.env.REACT_APP_GOOGLE_KEY
+        }`} loadingElement={<div style={{height: '100%'}} />} />
+  </div>
+);
 }
+
+
+
+
+
+
